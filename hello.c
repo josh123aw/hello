d13f0dd35ec0c3ff14382bdd00fc1ae0eccf70dd
josh <joshgab01@gmail.com>
2015-05-06 18:47:33 -0400

    adding hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+
+#include "stdio.h"
+#include "string.h"
+
+
+int main() 
+{ 
+		char buff[100];
+		printf("hello, what is your name?\n");
+		scanf("%s", buff);
+		if(strcmp(buff, "Josh") == 0) {
+			printf("Hello, %s\n", buff);
+		} else
+		{
+			printf("Hi, %s\n", buff);
+		}
+}
